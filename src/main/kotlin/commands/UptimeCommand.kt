f0c5331027434531ab777b7e70f739e0e94b7fbe
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-10-28 14:53:28 +0100

    minor bug fix

--- src/main/kotlin/commands/UptimeCommand.kt
+++ src/main/kotlin/commands/UptimeCommand.kt
@@ -6,7 +6,7 @@ import io.github.ydwk.ydwk.evm.event.events.interaction.SlashCommandEvent
 class UptimeCommand : SlashCommandExtender {
 
     override fun onSlashCommand(event: SlashCommandEvent) {
-        event.slash.channel?.sendMessage("Uptime: ${event.ydwk.uptime}ms")?.get()
+        event.slash.reply("Uptime: ${event.slash.ydwk.uptime}ms").get()
     }
 
     override val name: String
