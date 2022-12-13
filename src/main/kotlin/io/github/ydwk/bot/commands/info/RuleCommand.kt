62e8cefa0835a1e96f6b51e4103360d7a458ece7
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-12-13 18:00:25 +0000

    Update RuleCommand.kt

--- src/main/kotlin/io/github/ydwk/bot/commands/info/RuleCommand.kt
+++ src/main/kotlin/io/github/ydwk/bot/commands/info/RuleCommand.kt
@@ -30,7 +30,8 @@ class RuleCommand : SlashCommandExtender {
             .setDescription(
                 """
             
-            ## General Rules
+            ## General Rules ##
+            
             No blank nicknames.
             No inappropriate nicknames.
             No sexually explicit nicknames.
@@ -40,7 +41,7 @@ class RuleCommand : SlashCommandExtender {
             No inappropriate profile pictures.
             No sexually explicit profile pictures.
             No offensive profile pictures.
-            No membership granted to minors (under 18 years).
+            Must be older than 13 years old.
             Moderators reserve the right to change nicknames.
             Moderators reserve the right to use their own discretion regardless of any rule.
             No exploiting loopholes in the rules (please report them).
@@ -49,13 +50,17 @@ class RuleCommand : SlashCommandExtender {
             No inviting unofficial bots.
             No bugs, exploits, glitches, hacks, bugs, etc.
             
-            ### Text chat rules
+            ### Text chat rules ###
+            
             No spamming, swearing, or excessive cursing. (This includes emojis)
             No harassing, bullying, or threatening other members.
             No posting links to other servers.
             Dont say can I ask without asking a question.
             No posting links to other servers.
             Follow the topic of the channel you are in.
+            
+            ### Note ###
+            If you are under 13 years you will be banned if found out.
             
             Discord Terms of Service Apply: https://discord.com/terms
             Discord Community Guidelines Apply: https://discord.com/guidelines
